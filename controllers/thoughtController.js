1d6e7d152fa2e9b34437e4be9d6b3e3927267cdd
cjclap <dmxtechinn@gmail.com>
2022-11-13 22:56:35 -0600

    update controllers

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,1 +1,25 @@
-const { User, Thought } = require('../models');
+const { user, thought } = require('../models');
+
+module.exports = {
+    getThoughts(req, res) {
+
+    },
+    getThought(req, res) {
+
+    },
+    createThought(req, res) {
+
+    },
+    updateThought(req, res) {
+
+    },
+    deleteThought(req, res) {
+
+    },
+    addReaction(req, res) {
+
+    },
+    deleteReaction(req, res) {
+
+    },
+}
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,25 @@
-const { User, Thought } = require('../models');
+const { user, thought } = require('../models');
+
+module.exports = {
+    getUsers(req, res) {
+
+    },
+    getUser(req, res) {
+
+    },
+    createUser(req, res) {
+
+    },
+    updateUser(req, res) {
+
+    },
+    deleteUser(req, res) {
+
+    },
+    addFriend(req, res) {
+
+    },
+    deleteFriend(req, res) {
+
+    },
+}
